e2f07f75506562f1ac1bb1683da6ca67103ae5d1
Danish Pandita <49803908+DANISHPANDITA@users.noreply.github.com>
2021-01-07 19:30:52 +0530

    Update Slides.js

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React from "react";
 
-function Slides({slides}) {
-
-    return (
-        <div>
-            <div id="navigation" className="text-center">
-                <button data-testid="button-restart" className="small outlined">Restart</button>
-                <button data-testid="button-prev" className="small">Prev</button>
-                <button data-testid="button-next" className="small">Next</button>
-            </div>
-            <div id="slide" className="card text-center">
-                <h1 data-testid="title">Slide Title Here</h1>
-                <p data-testid="text">Slide Text Here</p>
-            </div>
-        </div>
-    );
-
+function Slides(props) {
+  const item = props.slides[this.props.index];
+  const index = props.index;
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button
+          onClick={props.resetSlides}
+          data-testid="button-restart"
+          className="small outlined"
+        >
+          Restart
+        </button>
+        <button
+          onClick={props.prevSlide}
+          disabled={index === 0}
+          data-testid="button-prev"
+          className="small"
+        >
+          Prev
+        </button>
+        <button
+          onClick={props.nextSlide}
+          disabled={index === props.slides.length - 1}
+          data-testid="button-next"
+          className="small"
+        >
+          Next
+        </button>
+      </div>
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{item.title}</h1>
+        <p data-testid="text">{item.text}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Slides;
