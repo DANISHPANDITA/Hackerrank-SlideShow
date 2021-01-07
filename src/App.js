95a6533a70839a88c80838ce325633c34f1811c7
Danish Pandita <49803908+DANISHPANDITA@users.noreply.github.com>
2021-01-07 19:31:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import React from 'react';
-import './App.css';
-import 'h8k-components';
+import React, { useState } from "react";
+import "./App.css";
+import "h8k-components";
 
-import Slides from './components/Slides';
+import Slides from "./components/Slides";
 const title = "Slideshow App";
 
-function App({slides}) {
-    return (
-        <div>
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="App">
-                <Slides slides={slides} />
-            </div>
-        </div>
-    );
+function App({ slides }) {
+  const Slides = (slides = [
+    {
+      title: "Today's workout plan",
+      text: "We're gonna do 3 fundamental exercises.",
+    },
+    {
+      title: "First, 10 push-ups",
+      text: "Do 10 reps. Remember about full range of motion. Don't rush.",
+    },
+    {
+      title: "Next, 20 squats",
+      text: "Squats are important. Remember to keep your back straight.",
+    },
+    {
+      title: "Finally, 15 sit-ups",
+      text: "Slightly bend your knees. Remember about full range of motion.",
+    },
+    {
+      title: "Great job!",
+      text: "You made it, have a nice day and see you next time!",
+    },
+  ]);
+  const [index, setindex] = useState(0);
+  const prevSlide = () => {
+    setindex((index - 1) % slides.length);
+  };
+
+  const nextSlide = () => {
+    setindex((index + 1) % slides.length);
+  };
+
+  const resetSlides = () => setindex(0);
+  return (
+    <div>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="App"></div>
+      <Slides
+        prevSlide={prevSlide}
+        nextSlide={nextSlide}
+        resetSlides={resetSlides}
+        slides={slides}
+        initialState={initialState}
+        index={state.index}
+      />
+    </div>
+  );
 }
 
 export default App;
